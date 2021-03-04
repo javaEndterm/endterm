911b5cd90ed280077a22b899c930dbc3d7e345e2
1august <Giant_Tuzik>
2021-03-04 18:43:57 +0600

    Change 3

--- src/com/company/data/DB.java
+++ src/com/company/data/DB.java
@@ -1,4 +1,24 @@
 package com.company.data;
 
-public class DB {
+import com.company.data.interfaces.IDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB implements IDB {
+    String login = "postgres";
+    String password = "Giant_Tuzik2002";
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        String connectionURL = "jdbc:postgresql://localhost:5432/postgres";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(connectionURL, login, password);
+            return connection;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
 }
